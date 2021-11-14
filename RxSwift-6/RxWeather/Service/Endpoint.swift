e1e06494cac31e8e07146ba9a361a40723661e6f
kei <kky0317@gmail.com>
2021-11-14 19:48:53 +0900

    update RxSwift 6 template

--- RxSwift-6/RxWeather/Service/Endpoint.swift
+++ RxSwift-6/RxWeather/Service/Endpoint.swift
@@ -24,12 +24,15 @@
 
 import Foundation
 import CoreLocation
+import RxSwift
 
 let summaryEndpoint = "https://api.openweathermap.org/data/2.5/weather"
 let forecastEndpoint = "https://api.openweathermap.org/data/2.5/forecast"
 
-func composeUrlRequest(endpoint: String, from location: CLLocation) -> URLRequest {
+func composeUrlRequest(endpoint: String, from location: CLLocation) -> Observable<URLRequest> {
     let urlStr = "\(endpoint)?lat=\(location.coordinate.latitude)&lon=\(location.coordinate.longitude)&appid=\(apiKey)&lang=kr&units=metric"
-    let url = URL(string: urlStr)!
-    return URLRequest(url: url)
+    
+    return Observable.just(urlStr)
+        .compactMap { URL(string: $0) }
+        .map { URLRequest(url: $0) }    
 }
